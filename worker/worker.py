f45fa83593f62ced705f1fb2ab551ad63bd34c96
Yang Yu <yang.yu9106@gmail.com>
2020-01-13 22:04:31 -0500

    working master-worker

--- worker/worker.py
+++ worker/worker.py
@@ -1,7 +1,9 @@
-import pika
-import time
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
+import time
+import socket
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq-service"))
 channel = connection.channel()
 
 channel.queue_declare(queue="task_queue", durable=True)
@@ -9,9 +11,9 @@ print(" [*] Waiting for messages. To exit press CTRL+C")
 
 
 def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
+    print(f'[x] Received {body} on hostname {socket.gethostname()}' )
     time.sleep(5)
-    print(" [x] Done")
+    print("[x] Done")
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
