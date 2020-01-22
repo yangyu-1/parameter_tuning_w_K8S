263da1c62fddfb1d090f2026305fa09bb0166302
Yang Yu <yyu@webstaurantstore.com>
2020-01-22 15:57:04 -0500

    trainning in worker done.

--- worker/trainer.py
+++ worker/trainer.py
@@ -1,11 +1,13 @@
 import json
 from xgboost import XGBRegressor
 from sklearn.model_selection import KFold, cross_val_score
+from sklearn.datasets import fetch_california_housing
 
 
 def trainer(param_grids: list, folds=10):
+    X, y = fetch_california_housing(return_X_y=True)
     for param in param_grids:
-        model = i.pop("model")
+        model = param.pop("model")
         if model == "xgboost":
             regressor = XGBRegressor(objective="reg:squarederror", **param)
             kfold = KFold(n_splits=folds)
--- worker/worker.py
+++ worker/worker.py
@@ -1,7 +1,9 @@
 import time
 import socket
 import pika
-from sklearn.datasets import fetch_california_housing
+from trainer import trainer
+import json
+
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq-service"))
 channel = connection.channel()
@@ -11,15 +13,12 @@ print(" [*] Waiting for messages. To exit press CTRL+C")
 
 
 def callback(ch, method, properties, body):
-    print(f"[x] Received {body} on hostname {socket.gethostname()}")
-    time.sleep(1)
-    print("[x] Done")
+    j_data = json.loads(body)
+    print(f"{j_data}")
+    print(f"{socket.gethostname()} ")
+    trainer(j_data)
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
-
-# j_data = json.loads(split[0])
-
-# X,y = fetch_california_housing(return_X_y=True)
 
 channel.basic_qos(prefetch_count=1)
 channel.basic_consume(queue="task_queue", on_message_callback=callback)
